5dab10af16896cee92e29c042d86a62f4acea376
Tianxiang Xiong <tianxiang.xiong@gmail.com>
2017-12-02 04:30:53 -0800

    Solve Day 1

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject aoc2017 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2017"
+  :url "https://adventofcode.com/2017"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
